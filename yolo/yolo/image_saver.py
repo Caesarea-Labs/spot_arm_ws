cab5cfe6e7eba216470dc007e2a9204445be094b
Lior-caesarealabs <lior.falach@caesarealabs.com>
2024-01-07 21:28:20 +0200

    pose inference publish in yolo package

--- yolo/yolo/image_saver.py
+++ yolo/yolo/image_saver.py
@@ -12,6 +12,8 @@ from cv_bridge import CvBridge
 from ament_index_python.packages import get_package_share_directory
 from yolov8_msgs.msg import InferenceResult
 from yolov8_msgs.msg import Yolov8Inference
+from yolov8_msgs.msg import Poseresults
+from yolov8_msgs.msg import Poseinference
 
 
 bridge = CvBridge()
@@ -26,16 +28,19 @@ class Camera_subscriber(Node):
         # Using cv2.imshow() method
         # Displaying the image
         model_file_detection = os.path.join(get_package_share_directory('yolo'),'models','best_detect.onnx')
-        self.model_detection = YOLO(model_file_detection)
+        self.model_detection = YOLO(model_file_detection,task='detect')
         model_file_pose = os.path.join(get_package_share_directory('yolo'), 'models', 'best_pose.onnx')
-        self.model_pose = YOLO(model_file_pose)
+        self.model_pose = YOLO(model_file_pose,task='pose')
         self.yolov8_inference = Yolov8Inference()
+        self.poseinference = Poseinference()
         print("YOLO model loaded")
         self.subscription = self.create_subscription(Image,'/camera/image_raw', self.camera_callback, 1)
         print("Image subscription created")
 
         self.YOLO_pub = self.create_publisher(Image,"/detection_results",1)
         self.yolov8_pub = self.create_publisher(Yolov8Inference, "/Yolov8_Inference", 1)
+        self.pose_pub = self.create_publisher(Poseinference, "/pose_inference", 1)
+        self.model_names=['chip','socket']
         # self.subscription
 
     def camera_callback(self, data):
@@ -44,6 +49,8 @@ class Camera_subscriber(Node):
         results = self.model_detection(img)
         self.yolov8_inference.header.frame_id = "inference"
         self.yolov8_inference.header.stamp = self.get_clock().now().to_msg()
+        self.poseinference.header.frame_id = 'pose_inference'
+        self.poseinference.header.stamp = self.get_clock().now().to_msg()
 
         for r in results:
             boxes = r.boxes
@@ -51,7 +58,7 @@ class Camera_subscriber(Node):
                 self.inference_result = InferenceResult()
                 b = box.xyxy[0].to('cpu').detach().numpy().copy()  # get box coordinates in (top, left, bottom, right) format
                 c = box.cls
-                self.inference_result.class_name = self.model.names[int(c)]
+                self.inference_result.class_name = self.model_names[int(c)]
                 self.inference_result.top = int(b[0])
                 self.inference_result.left = int(b[1])
                 self.inference_result.bottom = int(b[2])
@@ -69,9 +76,18 @@ class Camera_subscriber(Node):
         # cv2.imwrite('sample_out_1.png', img)
         # self.YOLO_pub.publish(bridge.cv2_to_imgmsg(res_img,'bgr8'))
         results_pose = self.model_pose(img)
-        print(results_pose)
-        res_pose = results_pose[0].plot()
-        self.YOLO_pub.publish(bridge.cv2_to_imgmsg(res_pose, 'bgr8'))
+        for c in range(len(results_pose[0].boxes.cls)):
+            self.pose_result = Poseresults()
+            self.pose_result.class_name = results_pose[0].names[int(results_pose[0].boxes.cls[c].numpy())]
+            self.pose_result.box = results_pose[0].boxes.xyxy[c,:].to('cpu').detach().numpy().copy().astype(int)
+            self.pose_result.keypointx = results_pose[0].keypoints.xy[c, :, 0].to('cpu').detach().numpy().copy().astype(int)
+            self.pose_result.keypointy = results_pose[0].keypoints.xy[c, :, 1].to('cpu').detach().numpy().copy().astype(int)
+            self.poseinference.pose_inference.append(self.pose_result)
+
+        self.pose_pub.publish(self.poseinference)
+        self.poseinference.pose_inference.clear()
+        # res_pose = results_pose[0].plot()
+        # self.YOLO_pub.publish(bridge.cv2_to_imgmsg(res_pose, 'bgr8'))
 
 
 
